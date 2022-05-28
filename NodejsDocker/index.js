9854880385e25470462e26a53e6fd09b195d3be8
Jones Kleber <visao@visao-ol.com.br>
2022-05-28 12:09:34 -0300

    1

--- NodejsDocker/index.js
+++ NodejsDocker/index.js
@@ -2,10 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Visão On-line API Gateway is running"));
+app.get("/", (req, res) => res.send("🖥️ Visão On-line API Gateway is running"));
 
 app.listen(3000);
-
 
 // executar o comando  para subir a aplicação
 // sudo docker-compose -f docker-compose.yml -f docker-production.yml up --build -d
