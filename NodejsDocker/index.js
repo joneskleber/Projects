e0b79940fcf699f1dd9d1fdeaa5c3e32c82cec86
Jones Kleber <visao@visao-ol.com.br>
2022-05-28 14:31:51 +0000

    Modifications on AWS Server

--- NodejsDocker/index.js
+++ NodejsDocker/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!!!"));
+app.get("/", (req, res) => res.send("Visão On-line API Gateway is running"));
 
 app.listen(3000);
+
+
+// executar o comando  para subir a aplicação
+// sudo docker-compose -f docker-compose.yml -f docker-production.yml up --build -d
